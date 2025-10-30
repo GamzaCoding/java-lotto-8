8436a1f626c4135351551f10c4f0cd28a3453847
seok <hyeongseok1995@gmail.com>
2025-10-30 17:10:33 +0900

    test(WinningLottoTest): 로또 번호 2개일치 기능 테스트 작성

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -66,4 +66,19 @@ public class WinningLottoTest {
         //then
         Assertions.assertThat(matchCount).isEqualTo(expectMatchCount);
     }
+
+    @Test
+    @DisplayName("로또 번호 2개가 일치한다.")
+    void 로또_번호_2개가_일치한다() {
+        //given
+        List<Integer> winningNumbers = List.of(1,2,42,43,44,45);
+        int bonusNumber = 10;
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int expectMatchCount = 2;
+        //when
+        int matchCount = winningLotto.calculate(lotto);
+        //then
+        Assertions.assertThat(matchCount).isEqualTo(expectMatchCount);
+    }
 }
