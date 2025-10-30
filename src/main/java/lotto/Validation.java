b1fa1f7596b30add782969801e54990c841150e9
seok <hyeongseok1995@gmail.com>
2025-10-30 15:03:23 +0900

    feat(Validation): 로또 구입 금액이 1,000원으로 나누어 떨어지지 않으면 예외 발생 기능 구현

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -9,8 +9,14 @@ public class Validation {
     }
 
     public static void validateNegative(String inputLottoPurchaseMoney) {
-        if(Integer.parseInt(inputLottoPurchaseMoney) < 0) {
+        if (Integer.parseInt(inputLottoPurchaseMoney) < 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 음수를 입력할 수 없습니다.");
+        }
+    }
+
+    public static void validateDivisibleByThousand(String inputLottoPurchaseMoney) {
+        if (Integer.parseInt(inputLottoPurchaseMoney) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야합니다..");
         }
     }
 }
