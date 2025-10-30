21d8dbdba5d5b935da22178babc7664aa4c32ce4
seok <hyeongseok1995@gmail.com>
2025-10-30 20:27:09 +0900

    test(LottoResultCalculatorTest): 2등 당첨 금액 구하는 기능 테스트 작성

--- src/test/java/lotto/LottoResultCalculatorTest.java
+++ src/test/java/lotto/LottoResultCalculatorTest.java
@@ -23,4 +23,22 @@ public class LottoResultCalculatorTest {
         //then
         assertThat(expectResultAmount).isEqualTo(resultAmount);
     }
+
+    @Test
+    @DisplayName("2등 당첨 금액을 구한다.")
+    void _2등_당첨_금액을_구한다() {
+        //given
+        int lottoMatchCount = 5;
+        boolean bonusCorrect = true;
+        LottoResultDto lottoResultDto = new LottoResultDto(lottoMatchCount, bonusCorrect);
+        LottoResultCalculator lottoResultCalculator = new LottoResultCalculator();
+        long expectResultAmount = 30_000_000;
+
+        //when
+        long resultAmount = lottoResultCalculator.calculateResultAmount(lottoResultDto);
+
+        //then
+        assertThat(expectResultAmount).isEqualTo(resultAmount);
+    }
+
 }
