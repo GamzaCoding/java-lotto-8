e62b0929b16d80481900611d1162fcb85c4c5e76
seok <hyeongseok1995@gmail.com>
2025-10-30 21:54:25 +0900

    test(RateOfReturnServiceTest): 총 수익률 구하는 기능 테스트 작성

--- src/test/java/lotto/LottoResultServiceTest.java
+++ src/test/java/lotto/LottoResultServiceTest.java
@@ -26,4 +26,25 @@ public class LottoResultServiceTest {
         //then
         Assertions.assertThat(expectTotalLottoWinningAmount).isEqualTo(totalLottoWinningAmount);
     }
+
+    @Test
+    @DisplayName("총 수익률을 알려준다.")
+    void 총_수익률을_알려준다() {
+        //given
+        WinningLotto winningLotto = new WinningLotto(List.of(10, 20, 30, 40, 44, 45), 1);
+        Lotto firstPriceLotto = new Lotto(List.of(10, 20, 30, 40, 44, 45));
+        Lotto secondPriceLotto = new Lotto(List.of(10, 20, 30, 40, 44, 1));
+        Lotto thirdPriceLotto = new Lotto(List.of(10, 20, 30, 40, 44, 2));
+
+        Lottos lottos = new Lottos(List.of(firstPriceLotto, secondPriceLotto, thirdPriceLotto));
+        LottoResultService lottoResultService = new LottoResultService(lottos, winningLotto);
+
+        long expectTotalLottoWinningAmount = 2_031_500_000;
+
+        //when
+        long totalLottoWinningAmount = lottoResultService.calculateTotalWinningAmount();
+
+        //then
+        Assertions.assertThat(expectTotalLottoWinningAmount).isEqualTo(totalLottoWinningAmount);
+    }
 }
---
+++ src/test/java/lotto/RateOfReturnServiceTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.math.BigDecimal;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RateOfReturnServiceTest {
+
+    @Test
+    @DisplayName("총 수익률을 알려준다")
+    void 총_수익률을_알려준다() {
+        //given
+        RateOfReturnService rateOfReturnService = new RateOfReturnService();
+        long inputLottoPurchaseMoney = 3_000;
+        long totalLottoWinningAmount = 2_031_500_000;
+        double expectedRateOfReturn = 677165.7;
+
+        //when
+        BigDecimal rateOfReturn = rateOfReturnService.calculateRateOfReturn(inputLottoPurchaseMoney, totalLottoWinningAmount);
+
+        //then
+        Assertions.assertThat(expectedRateOfReturn).isEqualTo(rateOfReturn.doubleValue());
+
+    }
+}
