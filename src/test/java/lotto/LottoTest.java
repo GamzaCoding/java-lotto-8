00d6a65977cd8dd7bc0e4f90433b4893c879aefa
seok <hyeongseok1995@gmail.com>
2025-10-30 14:09:38 +0900

    test(LottoTest): 로또 번호 숫자 0일 경우 예외 발생 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -42,4 +42,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
+
+    @Test
+    @DisplayName("로또 번호의 숫자가 0일 경우 예외가 발생한다.")
+    void 로또_번호의_숫자가_0일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
