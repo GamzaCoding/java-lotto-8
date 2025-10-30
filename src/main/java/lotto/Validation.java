4e63a6ae9b92f9bc4de77c4a41dd7e35d3307cdd
seok <hyeongseok1995@gmail.com>
2025-10-30 16:17:52 +0900

    feat(Validation): 로또 구입 금액 입력값이 0원인 경우 예외 발생 기능 구현

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -16,7 +16,13 @@ public class Validation {
 
     public static void validateDivisibleByThousand(String inputLottoPurchaseMoney) {
         if (Integer.parseInt(inputLottoPurchaseMoney) % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야합니다..");
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야합니다.");
+        }
+    }
+
+    public static void validateZeroNumber(String inputLottoPurchaseMoney) {
+        if (Integer.parseInt(inputLottoPurchaseMoney) == 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원을 입력할 수 없습니다.");
         }
     }
 }
