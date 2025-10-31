aebc780b1cc8a10c706099278d0c4f08be031d37
seok <hyeongseok1995@gmail.com>
2025-10-31 09:42:29 +0900

    feat(InputView): 입력기능 구현

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputPurchaseAmount() {
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber() {
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        return Console.readLine();
+    }
+}
